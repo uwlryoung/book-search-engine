48d7f48a91a4c0be0894b79165aed3e5998b5d59
Lucas Young <48228936+uwlryoung@users.noreply.github.com>
2023-10-14 16:43:14 -0400

    Added  inputTypes to the typeDefs file. Added type Auth, too.

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,32 +1,3 @@
-// TODO: Define the necessary Query and Mutation types: 
-// - Query type: 
-//    - me: Which returns a User type
-// - Mutation type: 
-//    - login: Accepts an email and password as parameters; returns an Auth type
-
-//    - addUser: Accepts a username, email, and password as parameters; returns an Auth type.
-
-//    - saveBook: Accepts a book author's array, description, title, bookId, image, and link as parameters; returns a User type. (Look into creating what's known as an input type to handle all of these parameters!)
-
-//    - removeBook: Accepts a book's bookId as a parameter; returns a User type. 
-
-// - User type: 
-//    - _id
-//    - username
-//    - email
-//    - bookCount
-//    - savedBooks (this will be an array of the Book type)
-// - Book type: 
-//    - bookId (Not the _id, but the book's id value returned from Google's Book API)
-//    - authors
-//    - description
-//    - title
-//    - image
-//    - link
-// - Auth type: 
-//    - token
-//    - user (References the User type)
-
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
@@ -36,30 +7,46 @@ const typeDefs = gql`
 
   type Mutatation {
     login(email: String!, password: String!): Auth
-    addUser(username: String!, email: String!, password: String!): Auth
-    saveBook(authors: Array!, description: String!, title: String!, bookId: String!, image: String, link: String ): User
+    addUser(input: UserInput, password: String!): Auth
+    saveBook(input: BookInput): User
     removeBook(bookId: String!): User
 
+  }
+
+  input UserInput {
+    username: String!
+    email: String!
   }
 
   type User {
     _id: ID!
     username: String!
-    password: String!
     email: String!
     bookCount: int
     savedBooks: [Book]
   }
 
-  type Book {
+  input BookInput {
     bookId: String!
-    authors: Array
+    authors: [String!]!
     description: String!
     title: String
     image: String
     link: String
+  }
 
+  type Book {
+    bookId: String!
+    authors: [String!]!
+    description: String!
+    title: String
+    image: String
+    link: String
+  }
 
+  type Auth {
+    token: String!
+    user: User
   }
 `;
 
